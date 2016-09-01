c2cea85254d5a3437a67787c0002fd15f2c8d0cb
Art4 <art4@wlabs.de>
2016-09-01 13:15:40 +0200

    Push requirements to PHP 5.6
    Add .gitignore and rename README
    Run tests with PHPUnit 5.5

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,12 +5,12 @@ ini_set('display_errors', 1);
 
 function includeIfExists($file)
 {
-    return file_exists($file) ? include $file : false;
+	return file_exists($file) ? include $file : false;
 }
 
 if (!includeIfExists(__DIR__.'/../vendor/autoload.php')) {
-    echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL;
-    exit(1);
+	echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+		'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
+		'php composer.phar install'.PHP_EOL;
+	exit(1);
 }
